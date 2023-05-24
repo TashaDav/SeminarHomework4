a5a7fee9c2964a7639a1e49d78ff48b908e5f4c0
davydova <natalya-davydova-86@mail.ru>
2023-05-24 21:36:29 +0300

    Created Task 027

---
+++ Task027/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+class Program
+{   
+    static void Main(string[] args)
+    {
+    Console.Write("Введите число: ");
+    var n = Convert.ToInt32(Console.ReadLine());
+    var sum = 0;
+    while(n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+    }
+}
