248ab6f50314ef766b68d242e2beffc25aab49d3
davydova <natalya-davydova-86@mail.ru>
2023-05-24 20:41:32 +0300

    Created Task 025

---
+++ Task025/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+int value = 2;
+for (int power = 1; power <=4; power++)
+Console.WriteLine($"{value}^{power} = {(long)Math.Pow(value, power)}");
