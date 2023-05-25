871763b3a83247ee543bf94ddb7e9f263d1315d6
davydova <natalya-davydova-86@mail.ru>
2023-05-25 10:24:31 +0300

    Chenged Task 025

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -2,6 +2,20 @@
 // 3, 5 -> 243
 // 2, 4 -> 16
 
+Console.WriteLine("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = A;
+
+for(int i = 1; i < B; i++)
+{
+    result = result * A;
+}
+Console.WriteLine(result);
+
+/*
 int value = 2;
 for (int power = 1; power <=4; power++)
 Console.WriteLine($"{value}^{power} = {(long)Math.Pow(value, power)}");
+*/
